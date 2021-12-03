e0ec36a7dfeb0203804473be7e05f4745e44cfe6
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-03 17:42:42 -0300

    Created the carousel for the banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './pages/components/Header';
+import Banner from './pages/components/Banner';
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
