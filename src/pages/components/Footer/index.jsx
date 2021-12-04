69c1bc14705c57a66276799ce7bab4c5ee55a772
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-03 23:11:19 -0300

    Updated header and footer anchor routes

--- src/pages/components/Footer/index.jsx
+++ src/pages/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/logo.svg';
 
 export default function Footer({ company }) {
   const generateDateString = () => {
-    const creationYear = '2021';
+    const creationYear = '2020';
     const currentYear = `${new Date().getFullYear()}`;
     return creationYear === currentYear
       ? currentYear
@@ -15,24 +15,24 @@ export default function Footer({ company }) {
       <Col md={{ span: 6, offset: 3 }} sm={{ span: 10, offset: 1 }}>
         <ListGroup as='ul' horizontal className='justify-content-center'>
           <ListGroup.Item as='li' className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='index.html' className='nav-link px-2 text-muted'>
               Home
             </a>
           </ListGroup.Item>
           <ListGroup.Item as='li' className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='index.html' className='nav-link px-2 text-muted'>
               Products
             </a>
           </ListGroup.Item>
           <ListGroup.Item as='li' className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='index.html' className='nav-link px-2 text-muted'>
               Team
             </a>
           </ListGroup.Item>
         </ListGroup>
       </Col>
       <p className='text-center text-muted'>
-        <img src={logo} alt='logo' style={{ width: '30px' }} /> {company} &copy;
+        <img src={logo} alt='logo' style={{ width: '20px' }} /> {company} &copy;
         Copyright {generateDateString()}
       </p>
     </Container>
--- src/pages/components/Header/index.jsx
+++ src/pages/components/Header/index.jsx
@@ -10,15 +10,15 @@ export default function Header() {
       <Navbar expand='lg' className='my-3'>
         <Container className='gridNavbar'>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Brand href='#home'>
+          <Navbar.Brand href='index.html'>
             <img src={logo} alt='logo' style={{ width: '40px' }} /> NeoTech
           </Navbar.Brand>
           <CgShoppingCart style={{ fontSize: '28px' }} className='gridItem' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Products</Nav.Link>
-              <Nav.Link href='#team'>Team</Nav.Link>
+              <Nav.Link href='index.html'>Home</Nav.Link>
+              <Nav.Link href='index.html'>Products</Nav.Link>
+              <Nav.Link href='index.html'>Team</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
