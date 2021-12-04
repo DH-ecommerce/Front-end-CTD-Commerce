a0faa595d26061fe1211d276165a0b126a47cf5a
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-03 22:37:47 -0300

    Fixed the issue with the cart

--- src/pages/components/Header/index.jsx
+++ src/pages/components/Header/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { CgShoppingCart } from 'react-icons/cg';
 import logo from '../../../assets/logo.svg';
+import './style.scss';
 
 export default function Header() {
   return (
     <>
-      <Navbar expand='lg'>
-        <Container>
+      <Navbar expand='lg' className='my-3'>
+        <Container className='gridNavbar'>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Brand href='#home'>
             <img src={logo} alt='logo' style={{ width: '40px' }} /> NeoTech
           </Navbar.Brand>
-          <CgShoppingCart style={{ fontSize: '28px' }} />
+          <CgShoppingCart style={{ fontSize: '28px' }} className='gridItem' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
               <Nav.Link href='#home'>Home</Nav.Link>
