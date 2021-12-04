5f73d9e34844fc8a50f1517947f3ec385be84d18
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-03 21:11:00 -0300

    Created the component Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './pages/components/Header';
 import Banner from './pages/components/Banner';
+import Footer from './pages/components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Footer company={'NeoTech'} />
     </>
   );
 }
