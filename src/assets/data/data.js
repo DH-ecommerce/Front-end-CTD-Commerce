a31b87b98c19b48f410573d1373a8d9cc9873d8f
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-04 00:20:47 -0300

    Fixed some issues

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,45 +2,61 @@ export const listProducts = [
   {
     id: '1',
     title: 'TMA-2 Comfort Wireless',
-    price: 1370.00,
-    description: 'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
-    image: ['https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
-    'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg', 'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg'],
+    price: 1370.0,
+    description:
+      'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
+    image: [
+      'https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg',
+    ],
     category: {
-      name: 'monitors'
+      name: 'monitors',
     },
   },
   {
     id: '2',
     title: 'TMA-2 Comfort Wireless',
-    price: 170.30,
-    description: 'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
-    image: ['https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
-    'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg', 'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg'],
+    price: 170.3,
+    description:
+      'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
+    image: [
+      'https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg',
+    ],
     category: {
-      name: 'monitors'
+      name: 'monitors',
     },
   },
   {
     id: '3',
     title: 'TMA-2 Comfort Wireless',
-    price: 599.90,
-    description: 'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
-    image: ['https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
-    'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg', 'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg'],
+    price: 599.9,
+    description:
+      'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
+    image: [
+      'https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg',
+    ],
     category: {
-      name: 'monitors'
+      name: 'monitors',
     },
   },
   {
     id: '4',
     title: 'TMA-2 Comfort Wireless',
-    price: 2100.00,
-    description: 'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
-    image: ['https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
-    'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg', 'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg'],
+    price: 2100.0,
+    description:
+      'Aumente a performance do seu computador com um ângulo de visão ultra-amplo de 178° tela de alto definição total de 1920 x 1080',
+    image: [
+      'https://m.media-amazon.com/images/I/812cGsOJhtL._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/81nlJd8Vk+L._AC_SL1500_.jpg',
+      'https://m.media-amazon.com/images/I/61hOjzvYzdL._AC_SL1500_.jpg',
+    ],
     category: {
-      name: 'monitors'
+      name: 'monitors',
     },
-  }
+  },
 ];
