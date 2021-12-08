48b5a1e8a5e093a92480901e1920012a678d5d69
sophiafiama <fiama.sophia.sf@gmail.com>
2021-12-08 11:14:16 -0300

    update scss

--- src/pages/components/Grid/CardProduct/index.jsx
+++ src/pages/components/Grid/CardProduct/index.jsx
@@ -21,19 +21,19 @@ export default function CardProduct({ id, img, title, price, category }) {
 
       <Container className='h-100 p-0 m-0' >
         <Card >
-          <Card.Img src={img} variant="top" style={{  maxHeight: '20vh', objectFit: 'contain', minHeight: '20vh'}} />
+          <Link className='no-style' to={`/products/${id}`}>
+            <Card.Img src={img} variant="top" style={{ maxHeight: '20vh', objectFit: 'contain', minHeight: '20vh' }} />
+          </Link>
           <Card.Body>
             <Card.Subtitle className="mb-2 text-muted category-subtitle">{category}</Card.Subtitle>
             <Card.Title><Link className='no-style' to={`/products/${id}`}>{title}</Link></Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">BRL {price}</Card.Subtitle>
+            <Card.Subtitle className="mb-2 text-muted">USD {price}</Card.Subtitle>
           </Card.Body>
-
           <Link className="no-style" to={`/products/${id}`}>
             <Card.Footer className="d-flex justify-content-between bg-white" >
               <div>
                 <BsFillStarFill className='mb-1' style={{ color: 'gold' }} /><small className="text-muted"> 4.7</small>
               </div>
-
               <div>
                 <small className="text-muted">38 Reviews<BsThreeDotsVertical /> </small>
               </div>
