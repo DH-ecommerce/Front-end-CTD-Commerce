ba47af3d6645b8656d5549c8d4908d81f76bba9a
Stefany Lovato <stefanylovato@gmail.com>
2021-12-09 14:08:30 -0300

    style ajustments

--- src/pages/SingleProduct/index.jsx
+++ src/pages/SingleProduct/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useViewport } from '../../hooks/ViewportProvider';
 import SingleProductDesktop from '../SingleProductDesktop/';
 import SingleProductMobile from '../SingleProductMobile/';
@@ -7,6 +7,11 @@ import ClientReview from '../components/ClientReview';
 function SingleProduct() {
   const { width } = useViewport();
   const breakpoint = 1025;
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return width < breakpoint ? (
     <>
       <SingleProductMobile />
