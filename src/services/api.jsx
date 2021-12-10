ad44a6699da2c72d9e4eaa76c41397bf2c48ad1d
Eduardo de Araújo Gomes Filho <73198111+eduardoaraujogomes@users.noreply.github.com>
2021-12-10 16:22:36 -0300

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://t2g1beanstalk-env.eba-q8yvkgmk.us-east-1.elasticbeanstalk.com',
-  httpsAgent:  new https.Agent({
-rejectUnauthorized: false
-})
+  baseURL: 'https://t2g1beanstalk-env.eba-q8yvkgmk.us-east-1.elasticbeanstalk.com'
 });
 
 export default api;
