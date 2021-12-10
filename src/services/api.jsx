fe8a3593536794ec0ba6292f67c0feca5319f177
Eduardo de Araújo Gomes Filho <73198111+eduardoaraujogomes@users.noreply.github.com>
2021-12-10 16:44:12 -0300

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    'https://neotechdh.herokuapp.com',
+    'https://t2g1beanstalk-env.eba-q8yvkgmk.us-east-1.elasticbeanstalk.com',
 });
 
 export default api;
